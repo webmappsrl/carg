e9cde9b3305ff607c833467ac093916b9ccabc70
bongiu <peppedeka@gmail.com>
2024-04-26 15:02:26 +0200

    feat: Add migration files for adding zip_url to sheets and creating jobs table
    - Added a new migration file (2024_04_24_111051_add_zip_url_to_sheets.php) to add a column for the URL of the ZIP file in the 'sheets' table.
    - Added another migration file (2024_04_24_113033_create_jobs_table.php) to create a new 'jobs' table with necessary columns.
    
    These changes will allow for storing and retrieving additional information related to sheets and managing job queues efficiently.

---
+++ database/migrations/2024_04_24_111051_add_zip_url_to_sheets.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('sheets', function (Blueprint $table) {
+            $table->string('file')->nullable(); // Aggiungi una colonna per l'URL dello ZIP
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('sheets', function (Blueprint $table) {
+            $table->dropColumn('file'); // Rimuovi la colonna
+        });
+    }
+};
