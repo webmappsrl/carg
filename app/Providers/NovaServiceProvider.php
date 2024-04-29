2fd34501a77952fe632488f98e012286260d7d73
bongiu <peppedeka@gmail.com>
2024-04-29 14:22:26 +0200

    feat: add new user to viewNova gate
    Add 'roberta.carta@isprambiente.it' as an authorized user to the viewNova gate in NovaServiceProvider.php.

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -60,7 +60,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                'admin@webmapp.it',
+                'admin@webmapp.it', 'roberta.carta@isprambiente.it'
             ]);
         });
     }
