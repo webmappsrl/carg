011b49285f5df7bc3bbf1d8f934c438b73e64822
bongiu <peppedeka@gmail.com>
2024-04-29 10:15:08 +0200

    chore: Refactor ProcessZipFromUrl.php
    - Removed unused method handle2()
    - Updated makeDirectory() calls to include recursive flag
    - Replaced mergeContents2() method with mergeContents()

--- app/Jobs/ProcessZipFromUrl.php
+++ app/Jobs/ProcessZipFromUrl.php
@@ -61,31 +61,6 @@ class ProcessZipFromUrl implements ShouldQueue
         unlink($tempZipPath);
     }
 
-    public function handle2()
-    {
-        $tempZipPath = tempnam(sys_get_temp_dir(), 'zip');
-        file_put_contents($tempZipPath, fopen($this->zipUrl, 'r'));
-        $zip = new ZipArchive;
-
-        if ($zip->open($tempZipPath) === true) {
-            // Estrai lo ZIP in una directory temporanea
-            $tempDir = storage_path('app/tempZip');
-            $zip->extractTo($tempDir);
-            $zip->close();
-
-            // Merge dei contenuti dello ZIP con la cartella 'tiles'
-            $this->mergeContents($tempDir, Storage::disk('tgen'));
-
-            // Pulizia: rimuovi il file ZIP temporaneo
-            unlink($tempZipPath);
-
-            // Pulizia: rimuovi la directory temporanea
-            Storage::deleteDirectory('tempZip');
-        } else {
-            Log::error("Unable to open the ZIP file from URL: {$this->zipUrl}");
-        }
-    }
-
     protected function mergeContents($sourceDir, $disk)
     {
         $files = new RecursiveIteratorIterator(
@@ -100,7 +75,7 @@ class ProcessZipFromUrl implements ShouldQueue
             }
             if ($fileInfo->isDir()) {
                 // Tentativo di creare la directory, ignora se esiste già (il driver FTP potrebbe non supportare 'exists')
-                $disk->makeDirectory($relativePath);
+                $disk->makeDirectory($relativePath, 0755, true);
             } else {
                 // Verifica che l'estensione sia '.png'
                 if ($fileInfo->getExtension() === 'png') {
@@ -110,56 +85,11 @@ class ProcessZipFromUrl implements ShouldQueue
                     // Assicurati che la directory di destinazione esista (crea se non esiste)
                     $directoryPath = dirname($relativePath);
                     if (!$disk->exists($directoryPath)) {
-                        $disk->makeDirectory($directoryPath);  // Assicurati che il driver supporti questa operazione
+                        $disk->makeDirectory($directoryPath, 0755, true);  // Assicurati che il driver supporti questa operazione
                     }
 
                     // Usa Storage facade per scrivere il file nel disco configurato
                     $disk->put($relativePath, $contents);
-                }
-            }
-        }
-    }
-
-    protected function mergeContents2($sourceDir, $targetDir)
-    {
-        $files = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($sourceDir, RecursiveDirectoryIterator::SKIP_DOTS),
-            RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        foreach ($files as $fileInfo) {
-            // Ottieni il percorso relativo senza la cartella "Mapnik"
-            $relativePath = str_replace('Mapnik' . DIRECTORY_SEPARATOR, '', $files->getSubPathName());
-
-            // Costruisci il percorso di destinazione
-            $targetPath = $targetDir . DIRECTORY_SEPARATOR . $relativePath;
-
-            // Se è una directory e non è numerica, continua con il prossimo file
-            if ($fileInfo->isDir() && !is_numeric(basename($relativePath))) {
-                continue;
-            }
-
-            // Crea la directory di destinazione se non esiste
-            if ($fileInfo->isDir() && !file_exists($targetPath)) {
-                mkdir($targetPath, 0755, true);
-                continue;
-            }
-
-            // Per i file, verifica che l'estensione sia '.png'
-            if (!$fileInfo->isDir() && $fileInfo->getExtension() !== 'png') {
-                continue;
-            }
-
-            // Assicurati che la directory di destinazione esista
-            if (!$fileInfo->isDir()) {
-                $directoryPath = dirname($targetPath);
-                if (!file_exists($directoryPath)) {
-                    mkdir($directoryPath, 0755, true);
-                }
-
-                // Copia il file nella destinazione
-                if (!copy($fileInfo->getRealPath(), $targetPath)) {
-                    Log::error("Failed to copy {$fileInfo->getRealPath()} to {$targetPath}");
                 }
             }
         }
