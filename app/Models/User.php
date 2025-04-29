b54f00bb75837ff0e13a2a51d4fe7fb4c158ebfb
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2025-04-29 08:10:33 +0200

    Nova 5 update (#5)
    * feat: Update to nova 5 OC:5006
    - Updated dependencies in composer.json, including changing versions for `laravel/nova` and `ahmedkandel/nova-s3-multipart-upload`.
    - Added new migration to track `last_login_at` for users.
    - Adjusted middleware and configuration settings in various files for improved functionality.
    - Enhanced language translations in `en.json` for better user experience.
    
    * chore: Update deployment scripts for improved functionality
    
    * chore: Update GitHub Actions workflow to include recursive submodule checkout
    
    * chore: Update GitHub Actions workflow triggers to include 'workflow_dispatch'
    
    * chore: Add debugging step to GitHub Actions workflow for checking working directory and listing files
    
    * chore: Update GitHub Actions workflow triggers to include 'push'
    
    * chore: Remove debugging step from GitHub Actions workflow and add submodule initialization
    
    * chore: Add wm-package submodule to the project
    
    * chore: Update .env-example with default MAIL_FROM_ADDRESS and clean up GitHub Actions workflow by removing submodule initialization step

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Wm\WmPackage\Model\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
