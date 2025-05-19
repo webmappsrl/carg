a83b9c7fa2b15ee30194ae25b9004209e2da5433
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2025-05-19 14:08:19 +0200

    UGCs_implementation OC:5512 (#8)
    * feat: published related migrations from wm-package
    
    * feat: Update dependencies in composer.json and composer.lock for Laravel 11 compatibility
    
    - Upgraded Laravel framework from ^10.0 to ^11.0.
    - Updated Laravel Sanctum from ^3.0 to ^4.0 and Laravel Tinker from ^2.7 to ^2.9.
    - Adjusted versions for several packages including spatie/laravel-translatable, romanzipp/laravel-queue-monitor, and others.
    - Updated development dependencies to their latest versions.
    - Reflected changes in composer.lock to ensure consistency with composer.json.
    - Updated wm-package submodule reference.
    
    This commit enhances compatibility with Laravel 11 and ensures all dependencies are up to date.
    
    * refactor: Extend User model from WmUser
    
    - Changed the User model to extend from WmUser instead of Authenticatable.
    - This modification integrates the User model with the Wm package's user functionalities.
    
    * feat: Add new Nova resources for Media, UgcPoi, and UgcTrack
    
    - Introduced Media, UgcPoi, and UgcTrack classes extending from their respective Wm package classes.
    - Updated NovaServiceProvider to include new resources in the main menu and UGC section.
    
    * feat: published wm-package migrations
    
    * feat: Update docker-compose.yml.example to include Redis, Elasticsearch, Minio, and Mailpit services
    
    - Added Redis service for caching.
    - Introduced Elasticsearch service for search capabilities.
    - Included Minio for AWS S3 storage testing.
    - Added Mailpit for email testing.
    - Fixed indentation for extra_hosts in phpfpm service.
    
    * feat: Add Nova App class and update NovaServiceProvider with new Tools section
    
    - Introduced a new App class in the Nova namespace extending from WmNovaApp.
    - Updated NovaServiceProvider to include a new Tools section with external links to Horizon and Telescope, visible only to admin users.
    
    * fix: added jwt for user login through app
    
    * chore: Update wm-package submodule reference to latest commit 18a5c69
    
    * fix: fixed migrations
    
    * chore: Update PostGIS image version in docker-compose.yml.example
    
    - Changed PostGIS image from version 14-3.3 to 17-3.5-alpine for improved performance and compatibility.

--- database/migrations/2025_05_13_130556_z_add_foreign_keys_to_apps_table.php
+++ database/migrations/2025_05_13_130556_z_add_foreign_keys_to_apps_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -11,11 +10,8 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('sheets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('carg_code')->nullable();
-            $table->geography('geometry', 'MultiPolygon')->nullable();
+        Schema::table('apps', function (Blueprint $table) {
+            $table->foreign(['user_id'])->references(['id'])->on('users');
         });
     }
 
@@ -24,6 +20,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('sheets');
+        Schema::table('apps', function (Blueprint $table) {
+            $table->dropForeign('apps_user_id_foreign');
+        });
     }
 };
