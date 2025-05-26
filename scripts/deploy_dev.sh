8964ea08851406abda91b736fc43215598ad9d6e
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-05-26 12:53:13 +0200

    🚀 build(deploy): update deployment scripts to include submodule initialization
    This commit modifies both the development and production deployment workflows to include the `git submodule update --init --recursive` command. This ensures that all submodules are initialized and updated during the deployment process, preventing potential issues with missing submodule content.
    
    Additionally, the redundant submodule update command has been removed from the individual deployment scripts, streamlining the deployment process.

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -6,8 +6,6 @@ echo "Deployment started ..."
 # Enter maintenance mode or return true
 php artisan down
 
-git submodule update --init --recursive
-
 # Install composer dependencies
 composer install  --no-interaction --prefer-dist --optimize-autoloader
 
